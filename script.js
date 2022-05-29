cc95d590f63a1baaaf3cf160618ba26638dbe99e
Parsival Nascimento <92957914+Dev-Parsival@users.noreply.github.com>
2022-05-29 02:28:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const imc = () =>{
+  let calculadora = window.open('https://calculadoraimc.parsivalnascime.repl.co/');
+  return calculadora;
+}
+
+function cep(){
+window.open('https://consulta-cep.parsivalnascime.repl.co/');
+
+}
